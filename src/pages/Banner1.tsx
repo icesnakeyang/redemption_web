3df9f727afb2fa8a1f50e94a1223e183af264c54
liuyang <flow_ocean@yahoo.com>
2023-02-03 17:03:33 +0800

    progress

--- src/pages/Banner1.tsx
+++ src/pages/Banner1.tsx
@@ -2,12 +2,12 @@ const Banner1 = () => {
     return (
         <div>
             <div>
-                <img src='banner1.png' width="100%"/>
+                <img src='1.png' width="100%"/>
             </div>
 
             <div style={{display: 'flex', marginTop: 10, padding:10}}>
                 <div style={{}}>
-                    <img src='rm8.png' width="100%"/>
+                    <img src='2.png' width="100%"/>
                 </div>
                 <div style={{width:600, fontSize:12}}>
                     We appreciate your continuous support and would like to improve our service for you.
